686b67730eb8d851cbf499262ab99a578dfdc722
Willem van Hage <wrvhage@chameleon.local>
2012-05-17 16:19:16 +0200

    Added support for NULL values, removed lossy XML parser.

--- SPARQL/R/SPARQL.R
+++ SPARQL/R/SPARQL.R
@@ -5,13 +5,16 @@ sparqlns <- c('s'='http://www.w3.org/2005/sparql-results#')
 commonns <- c('xsd','<http://www.w3.org/2001/XMLSchema#>','rdf','<http://www.w3.org/1999/02/22-rdf-syntax-ns#>','rdfs','<http://www.w3.org/2000/01/rdf-schema#>','owl','<http://www.w3.org/2002/07/owl#>','skos','<http://www.w3.org/2004/02/skos/core#>','dc','<http://purl.org/dc/elements/1.1/>','foaf','<http://xmlns.com/foaf/0.1/>','wgs84','<http://www.w3.org/2003/01/geo/wgs84_pos#>')
 
 sparqltest <- function(...) {
-  SPARQL('http://semanticweb.cs.vu.nl/lop/sparql/','SELECT ?et ?r ?at ?t WHERE { ?e sem:eventType ?et . ?e sem:hasActor ?a . ?a sem:actorType ?at . ?e sem:hasPlace ?p . ?p eez:inPiracyRegion ?r . ?e sem:hasTimeStamp ?t . }',c('lop','<http://semanticweb.cs.vu.nl/poseidon/ns/instances/>','eez','<http://semanticweb.cs.vu.nl/poseidon/ns/eez/>'), ...)
+  SPARQL(url='http://semanticweb.cs.vu.nl/lop/sparql/',
+         query='SELECT ?et ?r ?at ?t WHERE { ?e sem:eventType ?et . ?e sem:hasActor ?a . ?a sem:actorType ?at . ?e sem:hasPlace ?p . ?p eez:inPiracyRegion ?r . ?e sem:hasTimeStamp ?t . }',
+         ns=c('lop','<http://semanticweb.cs.vu.nl/poseidon/ns/instances/>','eez','<http://semanticweb.cs.vu.nl/poseidon/ns/eez/>'),
+         ...)
 }
 
 #
 # Read SPARQL results from end-point
 #
-SPARQL <- function(url="http://localhost/", query="", update="", ns=NULL, param="", extra=NULL, format="xml", lossy=TRUE) {
+SPARQL <- function(url="http://localhost/", query="", update="", ns=NULL, param="", extra=NULL, format="xml") {
   if (!is.null(extra)) {
 	  extrastr <- paste('&', sapply(seq(1,length(extra)),
 			    function (i) { paste(names(extra)[i],'=',URLencode(extra[[i]]), sep="") }),
@@ -37,9 +40,8 @@ SPARQL <- function(url="http://localhost/", query="", update="", ns=NULL, param=
 	                                 namespaces=sparqlns,
 	                                 quote(xmlGetAttr(x, "name"))))			
 	      ns2 <- noBrackets(ns)
-	      res <- sapply(attrs, get_attr(attr, DOM, ns2, lossy), simplify=FALSE)
-	      df <- data.frame(res) # FIXME: fails when there are NULL values in the result table, like with incompatible UNIONS
-	      names(df) <- attrs
+	      res <- get_attr(attrs, DOM, ns2)
+	      df <- data.frame(res)
 	      rm(res)
 	      rm(DOM)
 	    }
@@ -67,7 +69,6 @@ readTSVstring <- function(text, ...)
    dfr
 }
 
-
 readCSVstring <- function(text, ...)
 {
    dfr <- read.csv(tc <- textConnection(text), ...)
@@ -75,66 +76,58 @@ readCSVstring <- function(text, ...)
    dfr
 }
 
-get_attr <- function(attr, DOM, ns, lossy) {
-  if (lossy) {
-    rv <- function(attr) {
-      unlist(sapply(getNodeSet(DOM,
-                               paste('//s:result/s:binding[@name="', attr, '"]/s:uri/text() ',
-                                     '| //s:result/s:binding[@name="', attr, '"]/s:bnode',
-                                     '| //s:result/s:binding[@name="', attr, '"]/s:literal/text()',
-                                     sep=""),
-                               namespaces=c('s'='http://www.w3.org/2005/sparql-results#')),
-                    function(x) {
-                      qnames(xmlValue(x), ns)
-                    },
-                    simplify=FALSE))
+get_attr <- function(attrs, DOM, ns) {
+  rs <- getNodeSet(DOM,'//s:result',namespaces=sparqlns)
+  t(sapply(rs,
+         function(r) { 
+           sapply(attrs,
+                  function(attr) {
+                    get_value(getNodeSet(xmlDoc(r),
+                                         paste('//s:binding[@name="',attr,'"]/*[1]',
+                                               sep=''),
+                                               namespaces=sparqlns)[[1]],
+                              ns)
+                    })
+         }))
+}
+
+get_value <- function(node,ns) {
+  # FIXME: very slow...
+  doc <- xmlDoc(node)
+  uri = xpathSApply(doc, '/s:uri', xmlValue, namespaces=sparqlns)
+  if(length(uri) == 0) {
+    literal = xpathSApply(doc, '/s:literal', xmlValue, namespaces=sparqlns)
+    if(length(literal) == 0) {
+      bnode = xpathSApply(doc, '/s:bnode', xmlValue, namespaces=sparqlns)
+      if (length(bnode) == 0) { # error
+          '***oops***'
+      } else { # found bnode
+        paste('_:genid', bnode, sep='')
+      }
+    } else { # found literal
+      lang = xpathApply(doc, '/s:literal', xmlGetAttr, "xml:lang", namespaces=sparqlns)
+      if(is.null(lang[[1]])) {
+        type = xpathApply(doc, '/s:literal', xmlGetAttr, "datatype", namespaces=sparqlns)
+        if(is.null(type[[1]])) {
+          paste('"', literal, '"', sep="")
+        } else {
+          qname = qnames(type, ns)
+          if(unlist(qname) == unlist(type))
+            paste('"', literal, '"^^<', qname, '>', sep="")
+          else
+            paste('"', literal, '"^^', qname, sep="")
+        }
+      } else {
+        paste('"', literal, '"@', lang, sep='')
+      }
     }
-  } else {
-    rv <- function(attr) {
-      as.factor(unlist(sapply(getNodeSet(DOM,
-                                         paste('//s:result/s:binding[@name="', attr, '"]/*[1]', sep=""),
-                                         namespaces=sparqlns),
-                              function(x) { # FIXME: very slow...
-                                node = xmlDoc(x)
-                                uri = xpathSApply(node, '/s:uri', xmlValue, namespaces=sparqlns)
-                                if(length(uri) == 0) {
-                                  literal = xpathSApply(node, '/s:literal', xmlValue, namespaces=sparqlns)
-                                  if(length(literal) == 0) {
-                                    bnode = xpathSApply(node, '/s:bnode', xmlValue, namespaces=sparqlns)
-                                    if (length(bnode) == 0) { # error
-                                      
-                                    } else { # found bnode
-                                      paste('_:genid', bnode, sep='')
-                                    }
-                                  } else { # found literal
-                                    lang = xpathApply(node, '/s:literal', xmlGetAttr, "xml:lang", namespaces=sparqlns)
-                                    if(is.null(lang[[1]])) {
-                                      type = xpathApply(node, '/s:literal', xmlGetAttr, "datatype", namespaces=sparqlns)
-                                      if(is.null(type[[1]])) {
-                                        paste('"', literal, '"', sep="")
-                                      } else {
-					qname = qnames(type, ns)
-					if(unlist(qname) == unlist(type))
-	                                        paste('"', literal, '"^^<', qname, '>', sep="")
-					else
-						paste('"', literal, '"^^', qname, sep="")
-                                      }
-                                    } else {
-                                      paste('"', literal, '"@', lang, sep='')
-                                    }
-                                  }
-                                } else { # found URI
-                                  qname = qnames(uri, ns)
-                                  if(qname == uri)
-				    paste('<', uri, '>', sep="")
-				  else
-				    qname
-                                }
-                              },
-                              simplify=FALSE)))
-    }
+  } else { # found URI
+    qname = qnames(uri, ns)
+    if(qname == uri)
+      paste('<', uri, '>', sep="")
+    else
+      qname
   }
-  rv
 }
 
 noBrackets <- function(ns) {
